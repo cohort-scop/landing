d9200e6ed9c00672ef85bf9b8c68c92548b61b29
rodesousa <dessroberto@gmail.com>
2025-11-25 19:59:03 +0100

    fix loop 307
    Signed-off-by: rodesousa <dessroberto@gmail.com>

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,7 +4,8 @@ import { createNavigation } from 'next-intl/navigation';
 export const routing = defineRouting({
   locales: ['fr', 'en'],
   defaultLocale: 'fr',
-  localePrefix: 'as-needed' // "/" affiche le contenu FR, "/en" pour anglais
+  localePrefix: 'as-needed', // "/" affiche le contenu FR, "/en" pour anglais
+  localeDetection: false // Empêche la redirection automatique basée sur le navigateur
 });
 
 export const { Link, redirect, usePathname, useRouter } =
